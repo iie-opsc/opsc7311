e2d8df7eff1d05d42c53474bfd7cf3183b553e1c
Hermien Pellissier <miena.moo@gmail.com>
2020-08-19 20:11:04 +0200

    Module Manual 2.1: Declaring and initialising fields for the ImageViews

--- LearningUnit3/StarSucks/app/src/main/java/za/ac/iie/opsc/starsucks/MainActivity.java
+++ LearningUnit3/StarSucks/app/src/main/java/za/ac/iie/opsc/starsucks/MainActivity.java
@@ -3,12 +3,27 @@ package za.ac.iie.opsc.starsucks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // declare fields for the ImageViews
+    private ImageView img_Sb1;
+    private ImageView img_Sb2;
+    private ImageView img_Sb3;
+    private ImageView img_Sb4;
+    private ImageView img_Sb5;
+    private ImageView img_Sb6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img_Sb1 = findViewById(R.id.img_sb1);
+        img_Sb2 = findViewById(R.id.img_sb2);
+        img_Sb3 = findViewById(R.id.img_sb3);
+        img_Sb4 = findViewById(R.id.img_sb4);
+        img_Sb5 = findViewById(R.id.img_sb5);
+        img_Sb6 = findViewById(R.id.img_sb6);
     }
 }
