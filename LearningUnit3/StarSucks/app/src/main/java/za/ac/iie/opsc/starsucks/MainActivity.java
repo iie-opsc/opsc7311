bba5b5121d086e9fd19e987d41a63b6ea623e219
Hermien Pellissier <miena.moo@gmail.com>
2020-08-20 20:32:31 +0200

    Module Manual 4.1: Implementing View.OnClickListener in MainActivity

--- LearningUnit3/StarSucks/app/src/main/java/za/ac/iie/opsc/starsucks/MainActivity.java
+++ LearningUnit3/StarSucks/app/src/main/java/za/ac/iie/opsc/starsucks/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     // declare fields for the ImageViews
     private ImageView img_Sb1;
@@ -16,6 +16,31 @@ public class MainActivity extends AppCompatActivity {
     private ImageView img_Sb4;
     private ImageView img_Sb5;
     private ImageView img_Sb6;
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.img_sb1:
+                Toast.makeText(MainActivity.this, "MMM Soy Latte", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.img_sb2:
+                Toast.makeText(MainActivity.this, "MMM Chocco Frapp", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.img_sb3:
+                Toast.makeText(MainActivity.this, "MMM Bottled Americano", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.img_sb4:
+                Toast.makeText(MainActivity.this, "MMM Rainbow Frapp", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.img_sb5:
+                Toast.makeText(MainActivity.this, "MMM Caramel Frapp", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.img_sb6:
+                Toast.makeText(MainActivity.this, "MMM Black Forest Frapp", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,47 +53,12 @@ public class MainActivity extends AppCompatActivity {
         img_Sb5 = findViewById(R.id.img_sb5);
         img_Sb6 = findViewById(R.id.img_sb6);
 
-        img_Sb1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "MMM Soy Latte", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        img_Sb2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "MMM Chocco Frapp", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        img_Sb3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "MMM Bottled Americano", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        img_Sb4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "MMM Rainbow Frapp", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        img_Sb5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "MMM Caramel Frapp", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        img_Sb6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "MMM Black Forest Frapp", Toast.LENGTH_SHORT).show();
-            }
-        });
+        img_Sb1.setOnClickListener(this);
+        img_Sb2.setOnClickListener(this);
+        img_Sb3.setOnClickListener(this);
+        img_Sb4.setOnClickListener(this);
+        img_Sb5.setOnClickListener(this);
+        img_Sb6.setOnClickListener(this);
     }
 }
 
