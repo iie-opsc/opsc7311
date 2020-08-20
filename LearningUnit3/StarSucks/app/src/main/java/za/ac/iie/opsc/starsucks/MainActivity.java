0760894d151251e5938ad5d0b63190532ef831aa
Hermien Pellissier <miena.moo@gmail.com>
2020-08-20 23:51:56 +0200

    Module Manual 4.8.4: Navigation drawer components now added to the main activity

--- LearningUnit3/StarSucks/app/src/main/java/za/ac/iie/opsc/starsucks/MainActivity.java
+++ LearningUnit3/StarSucks/app/src/main/java/za/ac/iie/opsc/starsucks/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_with_nav_drawer);
 
         order = new Order();
 
