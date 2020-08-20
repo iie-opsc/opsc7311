eee40dda62ee5c673553df2f3b4250a0e800d7c9
Hermien Pellissier <miena.moo@gmail.com>
2020-08-20 20:06:31 +0200

    Module Manual 3.2: Adding the smallheart.png asset. And creating the OrderDetailsActivity

---
+++ LearningUnit3/StarSucks/app/src/main/java/za/ac/iie/opsc/starsucks/OrderDetailsActivity.java
@@ -0,0 +1,14 @@
+package za.ac.iie.opsc.starsucks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details);
+    }
+}
