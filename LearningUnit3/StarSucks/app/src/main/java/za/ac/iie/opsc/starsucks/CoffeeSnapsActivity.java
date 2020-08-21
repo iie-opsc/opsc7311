4a5b1f61821e0f73560d613578e614360d59e59e
Hermien Pellissier <miena.moo@gmail.com>
2020-08-21 19:32:26 +0200

    Module Manual 4.9.3: Taking the Photo, now with permissions

--- LearningUnit3/StarSucks/app/src/main/java/za/ac/iie/opsc/starsucks/CoffeeSnapsActivity.java
+++ LearningUnit3/StarSucks/app/src/main/java/za/ac/iie/opsc/starsucks/CoffeeSnapsActivity.java
@@ -1,9 +1,13 @@
 package za.ac.iie.opsc.starsucks;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -17,6 +21,7 @@ public class CoffeeSnapsActivity extends AppCompatActivity {
     private FloatingActionButton fab;
     private ImageView imgCameraImage;
     private static final int REQUEST_IMAGE_CAPTURE = 0;
+    private static final int REQUEST_IMAGE_CAPTURE_PERMISSION = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +34,38 @@ public class CoffeeSnapsActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(i, REQUEST_IMAGE_CAPTURE);
+                // Check if we have camera permission
+                if (ActivityCompat.checkSelfPermission(CoffeeSnapsActivity.this,
+                        Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    final String[] permissions = {Manifest.permission.CAMERA};
+                    // Request permission - this is asynchronous
+                    ActivityCompat.requestPermissions(CoffeeSnapsActivity.this,
+                            permissions, REQUEST_IMAGE_CAPTURE_PERMISSION);
+                } else {
+                    // We have permission, so take the photo
+                    takePhoto();
+                }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_IMAGE_CAPTURE_PERMISSION &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED) {
+            // We have permission, so take the photo
+            takePhoto();
+        }
+    }
+
+    private void takePhoto() {
+        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, REQUEST_IMAGE_CAPTURE);
     }
 
     @Override protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
