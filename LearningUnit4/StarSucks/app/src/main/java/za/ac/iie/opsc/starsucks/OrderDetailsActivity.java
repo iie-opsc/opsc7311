1407827571d632e22b3955aa67d30add49053acb
Hermien Pellissier <miena.moo@gmail.com>
2020-08-21 21:44:03 +0200

    Module Manual 3.2. Writing to the database

--- LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc/starsucks/OrderDetailsActivity.java
+++ LearningUnit4/StarSucks/app/src/main/java/za/ac/iie/opsc/starsucks/OrderDetailsActivity.java
@@ -2,15 +2,27 @@ package za.ac.iie.opsc.starsucks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
 
 public class OrderDetailsActivity extends AppCompatActivity {
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    // add orders to the path
+    private DatabaseReference starSucksRef = database.getReference("orders");
 
     private EditText etCustomerName;
     private EditText etCustomerCell;
@@ -68,6 +80,56 @@ public class OrderDetailsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 IntentHelper.shareIntent(OrderDetailsActivity.this, orderedValue);
+            }
+        });
+
+        fabCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // create a calendar to get today's date
+                Calendar datePickerCalendar = Calendar.getInstance();
+                int year = datePickerCalendar.get(Calendar.YEAR);
+                int month = datePickerCalendar.get(Calendar.MONTH);
+                int day = datePickerCalendar.get(Calendar.DAY_OF_MONTH);
+
+                // show a datepicker, starting from today's date
+                DatePickerDialog ordersDatePicker = new DatePickerDialog(
+                        OrderDetailsActivity.this,
+                        android.R.style.Theme_Light_Panel,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int month, int dayOfMonth) {
+                                // set the date on the order once it is picked
+                                order.setOrderDate(year + "-" + month + "-" + dayOfMonth);
+                            }
+                        }, year, month, day);
+
+                ordersDatePicker.show();
+            }
+        });
+
+        fabCloud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String customerCell = etCustomerCell.getText().toString();
+                String customerName = etCustomerName.getText().toString();
+                // check that none if the data is missing
+                if (!TextUtils.isEmpty(customerName) && !TextUtils.isEmpty(customerCell) &&
+                        !TextUtils.isEmpty(order.getOrderDate()) &&
+                        !TextUtils.isEmpty(orderedValue)) {
+                    order.setProductName(orderedValue);
+                    order.setCustomerName(customerName);
+                    order.setCustomerCell(customerCell);
+
+                    // add the order to the list of orders
+                    starSucksRef.push().setValue(order);
+                }
+                else {
+                    // message for alerting the user if something is missing
+                    Toast.makeText(OrderDetailsActivity.this,
+                            "Please complete all fields", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
